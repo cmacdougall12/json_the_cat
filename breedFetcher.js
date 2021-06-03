dbd29ae5d6bc8f219118b1c4b1766a154e3396bf
Cam MacDougall <cammacdougall12@gmail.com>
2021-06-03 15:50:11 +0000

    refactored breedFetcher.js and added file index.js. Also added a test directory and test file to test function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,22 @@
 const request = require("request");
-const apiLink = "https://api.thecatapi.com/v1/breeds/search?q=";
-const breed = process.argv.slice(2);
 
-request(apiLink + breed, (error, response, body) => {
-  if (error !== null) {
-    console.log(
-      "Could not complete request. Error code, " + error.code + " recieved."
-    );
-  } else if (body === "[]") {
-    console.log(
-      `Please confirm breed name was entered correctly. Could not locate breed information for ${breed}`
-    );
-  } else {
-    const data = JSON.parse(body);
-    console.log(data[0].description);
-  }
-});
+const fetchBreedDescription = function (breed, callback) {
+  const apiLink = "https://api.thecatapi.com/v1/breeds/search?q=";
+
+  request(apiLink + breed, (error, response, body) => {
+    if (error !== null) {
+      callback(error.code, null);
+    } else if (body === "[]") {
+      callback(
+        error,
+        `Please confirm breed name was entered correctly. Could not locate breed information for ${breed}`
+      );
+    } else {
+      const data = JSON.parse(body);
+      const desc = data[0].description;
+      callback(null, desc);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
